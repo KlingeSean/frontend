0df8ce5c7fc0ace2993b58a77da1e053ee618f4f
Klingesean <sean@webburo-spring.nl>
2025-01-03 22:53:32 +0100

    initialize React app with Vite, add basic structure and styling, and implement movie favorites context

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,65 @@
+import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const MovieCard = ({ movie }) => {
+  const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+
+  const favorite = isFavorite(movie.id);
+  const onFavouriteClick = (e) => {
+    e.preventDefault();
+    if (favorite) {
+      removeFromFavorites(movie.id);
+    } else {
+      addToFavorites(movie);
+    }
+  };
+
+  const average = Math.round(movie.vote_average * 10);
+
+  const getScoreColor = (score) => {
+    if (score >= 70) return "#21d07a"; // Green
+    if (score >= 50) return "#d2d531"; // Orange
+    return "#db2360"; // Red
+  };
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn${favorite ? " active" : ""}`}
+            onClick={onFavouriteClick}
+          >
+            ❤️
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <div className="score-container">
+          <div className="score-circle">
+            <CircularProgressbar
+              value={average}
+              text={`${average}%`}
+              styles={buildStyles({
+                textColor: "#fff",
+                pathColor: getScoreColor(average),
+                trailColor: "#444",
+              })}
+            />
+          </div>
+          <p>User score</p>
+        </div>
+        <p>{movie.release_date?.split("-")[0]}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
